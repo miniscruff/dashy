d64b2ec0bd503dc2ba943c87ef95148f43dc7bbf
Ronnie Smith <halfpint1170@gmail.com>
2022-03-10 00:49:44 -0800

    add nord color scheme

--- resources/static/scripts.js
+++ resources/static/scripts.js
@@ -1,5 +1,5 @@
 window.addEventListener('DOMContentLoaded', async () => {
   const response = await fetch('/api/values');
   const values = await response.json();
-	console.log(values);
+  console.log(values);
 });
